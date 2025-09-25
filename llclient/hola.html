7ba8170cb7940a1aaa375c1a71f76007dd424bdd
Paugomilabarber <pgomila20959@iesjoanramis.org>
2025-09-25 09:30:15 +0200

    hola menorca

--- llclient/hola.html
+++ llclient/hola.html
@@ -2,4 +2,8 @@
 Hola, Ramis!
 =======
 Hola, Menorca!
+<<<<<<< HEAD
 >>>>>>> 9432538 (a)
+=======
+ 
+>>>>>>> 9c6afc1 (hola menorca)
--- llservidor/hola.pl
+++ llservidor/hola.pl
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 Hola, Ramis!
 =======
 Hola, Menorca!
 >>>>>>> 9432538 (a)
+=======
+Hola, Menorca! 
+>>>>>>> 9c6afc1 (hola menorca)
